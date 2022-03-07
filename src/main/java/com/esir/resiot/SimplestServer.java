41ceddf36fde6349fb8183ddbe62fb48ceaaceb3
MateoFontanel <fontanelmateo@gmail.com>
2022-03-07 17:22:06 +0100

    Ajout serveur basique

--- src/main/java/com/esir/resiot/SimplestServer.java
+++ src/main/java/com/esir/resiot/SimplestServer.java
@@ -4,7 +4,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 
 public class SimplestServer {
-
     public static void main(String[] args) throws Exception {
         Server server = new Server();
         ServerConnector http = new ServerConnector(server);
