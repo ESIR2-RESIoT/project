e42104322e5167d03ca12624581f9b7a0c6f97e6
MateoFontanel <fontanelmateo@gmail.com>
2022-04-26 16:11:49 +0200

    Ajout début traitements chenillard par le Thread

--- src/main/java/com/esir/resiot/KNXHandler.java
+++ src/main/java/com/esir/resiot/KNXHandler.java
@@ -1,6 +1,5 @@
 package com.esir.resiot;
 
-import com.google.gson.JsonElement;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 
@@ -8,29 +7,19 @@ import javax.websocket.RemoteEndpoint;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class KNXHandler {
 
     private static final Logger LOG = Log.getLogger(Websocket.class);
     private static final GsonBuilder builder = new GsonBuilder();
     private RemoteEndpoint.Async remote;
 
-    private Boolean chaseState = false;
-    private Boolean chaser[] = {false,false,false,false};
-    private String chaseDirection = "ltr"; // left to right
-    private double chaseSpeed = 0.5;
+    private ThreadChenillard thread;
+
 
     public KNXHandler(RemoteEndpoint.Async remote) {
         this.remote = remote;
-    }
-    public KNXHandler() {
-        this.remote = null;
-    }
-
-    public void setRemote(RemoteEndpoint.Async remote) {
-        this.remote = remote;
+        thread = new ThreadChenillard(remote);
+        thread.start();
     }
 
     public void processRequest(String message){
@@ -51,46 +40,39 @@ public class KNXHandler {
         remote.sendText(message);
     }
 
-    /* Piste : Pour la partie JS utiliser un Web Worker pour faire tourner le chenillard en parallèle de KNX
-        Ainsi on envoie des données serveur->client uniquement pour confirmer les changements de paramètres
-        et pas à chaque changement d'état des LEDs
-    */
     private void changeState(boolean state){
         if(state){
-            //TODO: démarrer le thread chenillard
             LOG.info("Starting chaser");
             Gson gson = builder.create();
             boolean[] chaserArray = {true, false, false, false};
             String toSend = gson.toJson(chaserArray);
             this.remote.sendText(toSend);
-
         }else{
-            //TODO: récupérer l'état du chenillard puis arrêter le thread
             LOG.info("Stopping chaser");
             Gson gson = builder.create();
             boolean[] chaserArray = {false, true, false, false};
             String toSend = gson.toJson(chaserArray);
             this.remote.sendText(toSend);
         }
+
+        thread.changeThreadState();
     }
 
     private void changeDirection(){
-        this.chaseDirection = (this.chaseDirection.equals("ltr")) ? "rtl" : "ltr";
-        LOG.info("Changing direction to {}",this.chaseDirection);
-        //TODO: modifier la variable direction du thread chenillard
         Gson gson = builder.create();
         boolean[] chaserArray = {false, false, true, false};
         String toSend = gson.toJson(chaserArray);
         this.remote.sendText(toSend);
+
+        thread.changeChaserDirection();
     }
 
     private void changeSpeed(double speed){
-        this.chaseSpeed = speed;
-        LOG.info("Changing speed to {}",this.chaseSpeed);
-        //TODO: modifier la variable vitesse du thread chenillard
         Gson gson = builder.create();
         boolean[] chaserArray = {false, false, false, true};
         String toSend = gson.toJson(chaserArray);
         this.remote.sendText(toSend);
+
+        thread.changeChaserSpeed(speed);
     }
 }
